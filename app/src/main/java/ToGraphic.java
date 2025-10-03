39bd6ece631db974c1d03c956ce2eca46dfe8b8d
rabmarac_ <uros.krsteski@gmail.com>
2025-10-03 12:08:35 +0200

    Question 10

---
+++ app/src/main/java/ToGraphic.java
@@ -0,0 +1,33 @@
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.View;
+import androidx.annotation.Nullable;
+
+public class ToGraphic extends View {
+    public ToGraphic(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    @Override public void onDraw (Canvas canvas) {
+        Paint p = new Paint();
+        /*définir la couleur de l’objet de dessin */
+        p.setColor(Color.BLACK);
+        /*définir son style en remplissage*/
+        p.setStyle(android.graphics.Paint.Style.FILL);
+        /*dessiner un rectangle qui occupe la totalité du View*/
+        canvas.drawRect(0,0,getWidth(),getHeight(), p);
+        /*définir une autre couleur pour dessiner un texte*/
+        p.setColor(Color.GREEN);
+        /*définir la taille du texte*/
+        p.setTextSize(100);
+        /*définir le centre du texte comme étant son origine*/
+        p.setTextAlign(android.graphics.Paint.Align.CENTER);
+        /*dessiner le texte en positionnant son origine au centre du View */
+        String texte = "Bonjour MONDE";
+        canvas.drawText(texte, getWidth()/2, getHeight()/2, p);
+    }
+}
