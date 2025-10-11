557a299412bbd18d60cb11df96c692712241e0e6
rabmarac <uros.krsteski@gmail.com>
2025-10-11 17:04:46 +0200

    Complétion et fin du Timer v2

--- app/src/main/java/UPsay/decouverteAndroid/ToGraphic.java
+++ app/src/main/java/UPsay/decouverteAndroid/ToGraphic.java
@@ -10,6 +10,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -28,22 +29,66 @@ public class ToGraphic extends View {
         }
     };
 
+    /*
+    OnTouchListener onTouchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            float dx, dy;
+            float x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+            String direction;
+            switch(event.getAction()) {
+
+                case(MotionEvent.ACTION_DOWN):
+                    x1 = event.getX();
+                    y1 = event.getY();
+                    Log.i("pacman", "appuyé");
+                    break;
+                case(MotionEvent.ACTION_UP): {
+                    x2 = event.getX();
+                    y2 = event.getY();
+                    dx = x2-x1;
+                    dy = y2-y1;
+                    // Use dx and dy to determine the direction of the move
+                    if(Math.abs(dx) > Math.abs(dy)) {
+                        if(dx>0)
+                            direction = "right";
+                        else
+                            direction = "left";
+                    } else {
+                        if(dy>0)
+                            direction = "down";
+                        else
+                            direction = "up";
+                    }
+                    Log.i("pacman", "laché " + direction);
+                    Log.i("pacman", "dx = " + dx +"; dy = " + dy);
+                    break;
+                }
+            }
+            invalidate();
+            return true;
+        }
+    };
+    */
+
     /* Constructeur de la classe ToGraphic */
     public ToGraphic(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         timerHandler.postDelayed(updateTimerThread, 10);
+        // setOnTouchListener(onTouchListener);
     }
 
     // Overridable functions
     @Override public void onDraw (Canvas canvas) {
         @SuppressLint("DrawAllocation") Paint p = new Paint();
 
+        if (textSize > 100 ) textSize = 100;
         p.setColor(Color.BLACK);
-        p.setStyle(android.graphics.Paint.Style.FILL);
+        p.setStyle(Paint.Style.FILL);
         canvas.drawRect(0,0,getWidth(),getHeight(), p);
         p.setColor(Color.GREEN);
-        p.setTextSize(100);
-        p.setTextAlign(android.graphics.Paint.Align.CENTER);
+        p.setTextSize(textSize);
+        p.setTextAlign(Paint.Align.CENTER);
         String texte = "Bonjour MONDE";
 
         /* On créé le bitmap de l'image associer au jpg et on l'affiche avec drawBitmap */
